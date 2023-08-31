dfbee67d425a876f1b46c51798ac9def3be1aa54
MaxsterBlast <127631384+MaxsterBlast@users.noreply.github.com>
2023-08-31 15:47:01 -0600

    Update app.py
    Signed-off-by: MaxsterBlast <127631384+MaxsterBlast@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,19 +1,46 @@
-from flask import Flask, request
+from flask import Flask, render_template, request, redirect
+import json
 
 app = Flask(__name__)
 
-@app.route('/upload-paper', methods=['POST'])
-def upload_paper():
-    paper_data = request.form
-    # Process the paper data and save it to a text file
-    with open('received_data.txt', 'a') as f:
-        f.write(str(paper_data) + '\n')
-    return 'Data received and processed successfully.'
+papers = []
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+def save_data():
+    with open('data.json', 'w') as f:
+        json.dump(papers, f)
 
+def load_data():
+    try:
+        with open('data.json', 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
 
+@app.route('/')
+def index():
+    sorted_by_date = sorted(papers, key=lambda x: x['due_date'])
+    sorted_by_difficulty = sorted(papers, key=lambda x: x['difficulty'])
+
+    return render_template('index.html', papers=sorted_by_date, papers_by_difficulty=sorted_by_difficulty)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    paper = {
+        'subject': request.form['subject'],
+        'due_date': request.form['due_date'],
+        'style': request.form['style'],
+        'difficulty': request.form['difficulty'],
+        'completed': False
+    }
+    papers.append(paper)
+    save_data()
+    return redirect('/')
+
+@app.route('/delete/<int:index>')
+def delete(index):
+    papers.pop(index)
+    save_data()
+    return redirect('/')
 
 if __name__ == '__main__':
     papers = load_data()
