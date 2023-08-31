a5dbcd404033784045c09c254e3d59833f89cf42
MaxsterBlast <127631384+MaxsterBlast@users.noreply.github.com>
2023-08-31 09:29:40 -0600

    Update server.py
    Signed-off-by: MaxsterBlast <127631384+MaxsterBlast@users.noreply.github.com>

--- server.py
+++ server.py
@@ -1,21 +1,47 @@
-import os
-from flask import Flask, send_from_directory, render_template, redirect
+from flask import Flask, render_template, request, redirect
+import json
 
 app = Flask(__name__)
 
-port = int(os.environ.get("PORT", 5000))
+papers = []
 
-@app.route('/static/<path:path>')
-def serve_static(path):
-    return send_from_directory('static', path)
+def save_data():
+    with open('data.json', 'w') as f:
+        json.dump(papers, f)
+
+def load_data():
+    try:
+        with open('data.json', 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
 
 @app.route('/')
-def home():
-   return render_template('index.html')
+def index():
+    sorted_by_date = sorted(papers, key=lambda x: x['due_date'])
+    sorted_by_difficulty = sorted(papers, key=lambda x: x['difficulty'])
 
-@app.route('/<path:path>')
-def all_routes(path):
+    return render_template('index.html', papers=sorted_by_date, papers_by_difficulty=sorted_by_difficulty)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    paper = {
+        'subject': request.form['subject'],
+        'due_date': request.form['due_date'],
+        'style': request.form['style'],
+        'difficulty': request.form['difficulty'],
+        'completed': False
+    }
+    papers.append(paper)
+    save_data()
     return redirect('/')
 
-if __name__ == "__main__":
-    app.run(port=port)
+@app.route('/delete/<int:index>')
+def delete(index):
+    papers.pop(index)
+    save_data()
+    return redirect('/')
+
+if __name__ == '__main__':
+    papers = load_data()
+    app.run(port=5000)
