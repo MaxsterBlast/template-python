7a1cd5214d8fad68ad63b63f241aff3cba0b7db5
MaxsterBlast <127631384+MaxsterBlast@users.noreply.github.com>
2023-08-31 15:33:45 -0600

    Update app.py
    Signed-off-by: MaxsterBlast <127631384+MaxsterBlast@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,46 +1,22 @@
-from flask import Flask, render_template, request, redirect
-import json
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-
-papers = []
-
-def save_data():
-    with open('data.json', 'w') as f:
-        json.dump(papers, f)
-
-def load_data():
-    try:
-        with open('data.json', 'r') as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return []
+app.config['SECRET_KEY'] = '12345'
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    sorted_by_date = sorted(papers, key=lambda x: x['due_date'])
-    sorted_by_difficulty = sorted(papers, key=lambda x: x['difficulty'])
+    return render_template('index.html')
 
-    return render_template('index.html', papers=sorted_by_date, papers_by_difficulty=sorted_by_difficulty)
+@socketio.on('paper_uploaded')
+def handle_paper_upload(data):
+    # Here you can save the data to your computer
+    # For demonstration, print the data
+    print("Received paper data:", data)
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    paper = {
-        'subject': request.form['subject'],
-        'due_date': request.form['due_date'],
-        'style': request.form['style'],
-        'difficulty': request.form['difficulty'],
-        'completed': False
-    }
-    papers.append(paper)
-    save_data()
-    return redirect('/')
 
-@app.route('/delete/<int:index>')
-def delete(index):
-    papers.pop(index)
-    save_data()
-    return redirect('/')
+
 
 if __name__ == '__main__':
     papers = load_data()
