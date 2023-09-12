549ac5e2b2b30165a57d28229ab2e30d7c5a958d
MaxsterBlast <127631384+MaxsterBlast@users.noreply.github.com>
2023-09-11 18:53:11 -0600

    Update notes.js
    Signed-off-by: MaxsterBlast <127631384+MaxsterBlast@users.noreply.github.com>

--- templates/notes.js
+++ templates/notes.js
@@ -1,34 +1,64 @@
 // notes.js
 document.addEventListener('DOMContentLoaded', function () {
-    const noteInput = document.getElementById('note-input');
-    const saveButton = document.getElementById('save-button');
-    const noteList = document.getElementById('note-list');
+   const noteInput = document.getElementById('note-input');
+   const saveButton = document.getElementById('save-button');
+   const noteList = document.getElementById('note-list');
 
-    // Load saved notes from local storage if available
-    const savedNotes = JSON.parse(localStorage.getItem('notes')) || [];
+   // Initialize notes as an empty array
+   let notes = [];
 
-    // Display saved notes
-    savedNotes.forEach(function (note) {
-        const noteDiv = document.createElement('div');
-        noteDiv.textContent = note;
-        noteList.appendChild(noteDiv);
-    });
+   // Load saved notes from local storage if available
+   const savedNotes = JSON.parse(localStorage.getItem('notes'));
 
-    // Save a new note
-    saveButton.addEventListener('click', function () {
-        const newNote = noteInput.value.trim();
+   if (savedNotes) {
+       notes = savedNotes;
+       // Display saved notes
+       savedNotes.forEach(function (note, index) {
+           createNoteElement(note, index);
+       });
+   }
 
-        if (newNote !== '') {
-            const noteDiv = document.createElement('div');
-            noteDiv.textContent = newNote;
-            noteList.appendChild(noteDiv);
+   // Function to create a note element
+   function createNoteElement(note, index) {
+       const noteDiv = document.createElement('div');
+       noteDiv.textContent = note;
 
-            // Save the note to local storage
-            savedNotes.push(newNote);
-            localStorage.setItem('notes', JSON.stringify(savedNotes));
+       const editButton = document.createElement('button');
+       editButton.textContent = 'Edit';
+       editButton.addEventListener('click', function () {
+           const updatedNote = prompt('Edit the note:', note);
+           if (updatedNote !== null) {
+               notes[index] = updatedNote;
+               localStorage.setItem('notes', JSON.stringify(notes));
+               noteDiv.textContent = updatedNote;
+           }
+       });
 
-            // Clear the input field
-            noteInput.value = '';
-        }
-    });
+       const deleteButton = document.createElement('button');
+       deleteButton.textContent = 'Delete';
+       deleteButton.addEventListener('click', function () {
+           if (confirm('Are you sure you want to delete this note?')) {
+               notes.splice(index, 1);
+               localStorage.setItem('notes', JSON.stringify(notes));
+               noteDiv.remove();
+           }
+       });
+
+       noteDiv.appendChild(editButton);
+       noteDiv.appendChild(deleteButton);
+
+       noteList.appendChild(noteDiv);
+   }
+
+   // Save a new note
+   saveButton.addEventListener('click', function () {
+       const newNote = noteInput.value.trim();
+
+       if (newNote !== '') {
+           notes.push(newNote);
+           localStorage.setItem('notes', JSON.stringify(notes));
+           createNoteElement(newNote, notes.length - 1);
+           noteInput.value = '';
+       }
+   });
 });
