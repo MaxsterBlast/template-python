8887981df7143a82cea56059a896eae0c45bb812
MaxsterBlast <127631384+MaxsterBlast@users.noreply.github.com>
2023-11-09 18:07:15 -0600

    Create Setup.py
    Signed-off-by: MaxsterBlast <127631384+MaxsterBlast@users.noreply.github.com>

---
+++ Setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='citation-formatter',
+    version='1.0.0',
+    description='A citation formatting library',
+    author='Your Name',
+    author_email='your@email.com',
+    packages=['citation_formatter'],
+)
